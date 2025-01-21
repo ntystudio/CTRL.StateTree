b6e94527e6855f0eba9a72bcf90080477a255095
Tim Oxley <secoif@gmail.com>
2025-01-21 17:28:06 -0500

    Add Spawn Actor task.

---
+++ Source/ExtendedStateTree/Tasks/EEstSpawnActor.cpp
@@ -0,0 +1,63 @@
+﻿// SPDX-FileCopyrightText: 2025 NTY.studio
+
+#include "ExtendedStateTree/Tasks/EEstSpawnActor.h"
+
+#include "StateTreeExecutionContext.h"
+
+#include "Engine/World.h"
+
+#include "ExtendedStateTree/ExtendedStateTree.h"
+
+#include "Kismet/KismetMathLibrary.h"
+
+EStateTreeRunStatus FEstSpawnActor::EnterState(FStateTreeExecutionContext& Context, FStateTreeTransitionResult const& Transition) const
+{
+	FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+	if (InstanceData.SpawnedActor) {
+		return EStateTreeRunStatus::Running;
+	}
+	if (!InstanceData.ActorClass)
+	{
+		EST_LOG(Error, TEXT("Failed to spawn actor: No Actor Class."));
+		return EStateTreeRunStatus::Failed;
+	}
+	auto World = Context.GetWorld();
+	if (!World)
+	{
+		EST_LOG(Error, TEXT("Failed to spawn actor: No World."));
+		return EStateTreeRunStatus::Failed;
+	}
+	FActorSpawnParameters SpawnParameters;
+	SpawnParameters.Owner = InstanceData.Owner;
+	SpawnParameters.Instigator = InstanceData.Instigator;
+	SpawnParameters.SpawnCollisionHandlingOverride = InstanceData.CollisionHandlingMethod;
+	FTransform Transform = FTransform(InstanceData.SpawnRotation, InstanceData.SpawnLocation, InstanceData.SpawnScale);
+	auto Rotation = Transform.Rotator();
+	Rotation.Yaw += UKismetMathLibrary::RandomIntegerInRange(-InstanceData.RandomYaw * 0.5, InstanceData.RandomYaw * 0.5);
+	Transform.SetRotation(Rotation.Quaternion());
+	auto SpawnedActor = World->SpawnActor(InstanceData.ActorClass, &Transform, SpawnParameters);
+	if (!SpawnedActor)
+	{
+		EST_LOG(Error, TEXT("Failed to spawn actor: %s"), *InstanceData.ActorClass.Get()->GetDisplayNameText().ToString())
+		return EStateTreeRunStatus::Failed;
+	}
+
+	InstanceData.SpawnedActor = SpawnedActor;
+	return EStateTreeRunStatus::Running;
+}
+
+void FEstSpawnActor::ExitState(FStateTreeExecutionContext& Context, FStateTreeTransitionResult const& Transition) const
+{
+	FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+	if (InstanceData.bDestroyOnExit && IsValid(InstanceData.SpawnedActor))
+	{
+		auto const SpawnedActor = InstanceData.SpawnedActor;
+		InstanceData.SpawnedActor = nullptr;
+		SpawnedActor->Destroy();
+	}
+}
+
+FText FEstSpawnActor::GetDescription(FGuid const& ID, FStateTreeDataView InstanceDataView, IStateTreeBindingLookup const& BindingLookup, EStateTreeNodeFormatting Formatting) const
+{
+	return FEstStateTreeTaskCommonBase::GetDescription(ID, InstanceDataView, BindingLookup, Formatting);
+}
---
+++ Source/ExtendedStateTree/Tasks/EEstSpawnActor.h
@@ -0,0 +1,78 @@
+﻿// SPDX-FileCopyrightText: 2025 NTY.studio
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CoreMinimal.h"
+#include "StateTreeTaskBase.h"
+
+#include "ExtendedStateTree/Tasks/EstStateTreeTaskCommonBase.h"
+
+#include "Templates/SubclassOf.h"
+
+#include "EEstSpawnActor.generated.h"
+
+enum class ESpawnActorCollisionHandlingMethod : uint8;
+
+USTRUCT(BlueprintType)
+struct FEstSpawnActorData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TSubclassOf<AActor> ActorClass;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FVector SpawnLocation = FVector::ZeroVector;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FRotator SpawnRotation = FRotator::ZeroRotator;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FVector SpawnScale = FVector::OneVector;;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	int32 RandomYaw = 0;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	ESpawnActorCollisionHandlingMethod CollisionHandlingMethod;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TObjectPtr<AActor> Owner;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TObjectPtr<APawn> Instigator;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	bool bDestroyOnExit = false;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Output")
+	TObjectPtr<AActor> SpawnedActor;
+};
+
+USTRUCT(BlueprintType, DisplayName="Spawn Actor [EST]")
+struct EXTENDEDSTATETREE_API FEstSpawnActor : public FEstStateTreeTaskCommonBase
+{
+	GENERATED_BODY()
+
+public:
+	using FInstanceDataType = FEstSpawnActorData;
+	virtual UStruct const* GetInstanceDataType() const override { return FInstanceDataType::StaticStruct(); }
+	virtual EStateTreeRunStatus EnterState(FStateTreeExecutionContext& Context, FStateTreeTransitionResult const& Transition) const override;
+	virtual void ExitState(FStateTreeExecutionContext& Context, FStateTreeTransitionResult const& Transition) const override;
+
+#if WITH_EDITOR
+	virtual FText GetDescription(
+		FGuid const& ID,
+		FStateTreeDataView InstanceDataView,
+		IStateTreeBindingLookup const& BindingLookup,
+		EStateTreeNodeFormatting Formatting = EStateTreeNodeFormatting::Text
+	) const override;
+
+	virtual FName GetIconName() const override
+	{
+		return FName("EditorStyle|Icons.Visibility");
+	}
+
+#endif
+};
